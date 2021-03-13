70deb54c94876f6f94d8ceab51b0137ec2c24bec
benvaro <benvaro91@gmail.com>
2021-03-13 11:26:33 +0200

    Add project files.

---
+++ 01_Wpf_Intro/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_Wpf_Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn.Background = Brushes.Blue;
+
+            var btn3 = new Button()
+            {
+                Content = "Custom btn",
+                FontSize = 30
+            };
+
+            //  Canvas.SetBottom(btn3, 20);
+            Grid.SetColumn(btn3, 1);
+            Grid.SetRow(btn3, 0);
+            panel.Children.Add(btn3);
+        }
+    }
+}
