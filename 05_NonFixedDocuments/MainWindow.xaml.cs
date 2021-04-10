adf2f428ab56d5b57964fc6839dbd5067fd376f7
benvaro <benvaro91@gmail.com>
2021-04-10 14:35:59 +0300

    Non fixed documents. Bindings

---
+++ 05_NonFixedDocuments/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05_NonFixedDocuments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var run = new Run(txtBox.Text);
+            var bold = new Bold(run);
+
+            var paragraph = new Paragraph();
+            paragraph.Inlines.Add(bold);
+            paragraph.FontSize = 40;
+
+            var doc = new FlowDocument();
+            doc.Blocks.Add(paragraph);
+
+            reader.Document = doc;
+        }
+    }
+}
---
+++ 06_Binding/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> FruitsSimple { get; set; } = new List<string>();
+        public ObservableCollection<string> Fruits { get; set; } = new ObservableCollection<string>();
+
+        public CustomColor CustomColor { get; set; } = new CustomColor();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var binding = new Binding
+            {
+                ElementName = "slider2", // 
+                Path = new PropertyPath("Value"),
+                Mode = BindingMode.TwoWay
+            };
+
+            lbl2.SetBinding(Label.FontSizeProperty, binding);
+
+            // set foreground of the label via color entered into the textBox
+            var binding2 = new Binding
+            {
+                ElementName = "txtColor",
+                Path = new PropertyPath("Text"),
+                Mode = BindingMode.TwoWay
+            };
+
+            lbl2.SetBinding(Label.ForegroundProperty, binding2);
+
+            FruitsSimple.AddRange(new[] { "Apple", "Banana" });
+
+            Fruits.Add("Apple");
+            Fruits.Add("Cherry");
+            box2.DataContext = Fruits;
+            box1.DataContext = FruitsSimple;
+
+            CustomColor.Blue = CustomColor.Green = CustomColor.Red = 100;
+            this.DataContext = CustomColor;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            lbl1.FontSize = 20;
+        }
+
+        private void AddSimple(object sender, RoutedEventArgs e)
+        {
+            FruitsSimple.Add("Banana");
+        }
+        private void AddUpdatable(object sender, RoutedEventArgs e)
+        {
+            Fruits.Add("Banana");
+        }
+}
+}
