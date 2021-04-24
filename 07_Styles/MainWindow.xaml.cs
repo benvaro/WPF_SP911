93e98b16d7442e46c3bd7af2e148c9bcb4b3b784
benvaro <benvaro91@gmail.com>
2021-04-24 11:21:46 +0300

    Styles, Resourses

---
+++ 07_Styles/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_Styles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var other = new OtherWindow();
+            other.Show();
+        }
+    }
+}
---
+++ 08_PagesLocalization/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _08_PagesLocalization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDictionary("ua");
+        }
+
+        private void changeLang(object sender, RoutedEventArgs e)
+        {
+            LoadDictionary((sender as MenuItem).Tag.ToString());
+        }
+
+        private void LoadDictionary(string name)
+        {
+            var dictionary = new ResourceDictionary();
+            dictionary.Source = new Uri("Localization/" + name + ".xaml", UriKind.Relative);
+
+            this.Resources.MergedDictionaries.Clear();
+            this.Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
